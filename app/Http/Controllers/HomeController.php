fc830c519f432b5c7542500ac4b7ba0a5e722551
Johan <frst008@gmail.com>
2016-05-27 16:12:39 -0600

    Pregunta-3-4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,19 +6,23 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Pregunta1;
 use App\Pregunta2;
+use App\Pregunta3;
+use App\Pregunta4;
+
 class HomeController extends Controller
 {
-  protected $p1, $p2;
+  protected $p1, $p2, $p3, $p4;
   /**
   * Create a new controller instance.
   *
   * @return void
   */
-  public function __construct(Pregunta1 $p1, Pregunta2 $p2)
+  public function __construct(Pregunta1 $p1, Pregunta2 $p2, Pregunta3 $p3, Pregunta4 $p4)
   {
     $this->p1 = $p1;
     $this->p2 = $p2;
-    
+    $this->p3 = $p3;
+    $this->p4 = $p4;
   }
 
   /**
@@ -28,41 +32,69 @@ class HomeController extends Controller
   */
   public function index()
   {
-
-    $total = $this->p1->pluck('val1');
-    $total1 = $this->p1->pluck('val2');
-    $total2 = $this->p1->pluck('val3');
-    //
-    $t3 = ["name"=> "Si", "y" => $total[0]];
-    $t4 = ["name"=> "No", "y" => $total1[0]];
-    $t5 = ["name"=> "Algunas veces", "y" => $total2[0]];
+    $r1 = ["name"=> "Si", "y" => $this->p1->pluck('val1')[0]*100];
+    $r2 = ["name"=> "No", "y" => $this->p1->pluck('val2')[0]*100];
+    $r3 = ["name"=> "Algunas veces", "y" => $this->p1->pluck('val3')[0]*100];
     //dd($total->toArray());
-    $yourFirstChart["chart"] = array("type" => "pie");
-    $yourFirstChart["title"] = array("text" =>
+    $pregunta1["chart"] = array("type" => "pie");
+    $pregunta1["title"] = array("text" =>
     "¿Alguna vez ha tenido problemas al comunicarse con alguna persona con limitaciones verbales?");
-    $yourFirstChart["tooltip"] = array("pointFormat" =>  "{series.name}: <b>{point.percentage:.1f}%</b>");
-    $yourFirstChart["plotOptions"]  = array(
+    $pregunta1["tooltip"] = array("pointFormat" =>  "{series.name}: <b>{point.percentage:.1f}%</b>");
+    $pregunta1["plotOptions"]  = array(
       "pie" => array(
         "allowPointSelect" => true,
         "cursor" => "pointer",
         "dataLabels" => array("enabled" => false),
         "showInLegend" => "true")
       );
-      $yourFirstChart["series"] = [
-        array("name" => "Respuestas", "data" => [$t3, $t4, $t5]),
+      $pregunta1["series"] = [
+        array("name" => "Respuestas", "data" => [$r1, $r2, $r3]),
       ];
-      $hola = "Hola mundo";
       //---------------------------------------------------------------------------------------------------------------------------
+      $r4 = ["name"=> "Si", "y" => $this->p2->pluck('val1')[0]*100];
+      $r5 = ["name"=> "No", "y" => $this->p2->pluck('val2')[0]*100];
+      $pregunta2["chart"] = ["type" => "pie"];
+      $pregunta2["title"] = ["text" => "¿Conoce algún caso de una persona con parálisis que le impida hablar o comunicarse?"];
+      $pregunta2["tooltip"] = ["pointFormat" =>  "{series.name}: <b>{point.percentage:.1f}%</b>"];
+      $pregunta2["plotOptions"]  = [
+        "pie" => [
+          "allowPointSelect" => true,
+          "cursor" => "pointer",
+          "dataLabels" => ["enabled" => false],
+          "showInLegend" => "true"]
+        ];
+        $pregunta2["series"] = [["name" => "Respuestas", "data" => [$r4, $r5]]];
+        //---------------------------------------------------------------------------------------------------------------------------
+        $r6 = ["name"=> "Si", "y" => $this->p3->pluck('val1')[0]*100];
+        $r7 = ["name"=> "No", "y" => $this->p3->pluck('val2')[0]*100];
+        $pregunta3["chart"] = ["type" => "pie"];
+        $pregunta3["title"] = ["text" => "¿Está de acuerdo en que se crea un dispositivo capaz de escribir en una pantalla los pensamientos de una persona?"];
+        $pregunta3["tooltip"] = ["pointFormat" =>  "{series.name}: <b>{point.percentage:.1f}%</b>"];
+        $pregunta3["plotOptions"]  = [
+          "pie" => [
+            "allowPointSelect" => true,
+            "cursor" => "pointer",
+            "dataLabels" => ["enabled" => false],
+            "showInLegend" => "true"]
+          ];
+          $pregunta3["series"] = [["name" => "Respuestas", "data" => [$r6, $r7]]];
 
-      $t1 = $this->p2->pluck('val1');
-      $t2 = $this->p2->pluck('val2');
-      $yourSecondChart["chart"] = array("type" => "bar");
-      $yourSecondChart["title"] = array("text" =>
-      "¿Conoce algún caso de una persona con parálisis que le impida hablar o comunicarse?");
-      $yourSecondChart["xAxis"] = array("categories" => ['Si', 'NO', 'GG']);
-      $yourSecondChart["yAxis"] = array("title" => array("text" => "Fruit eaten"));
-      $yourSecondChart["series"] = [array("name" => "Respuestas", "data" => [$t1, $t2])];
+          //---------------------------------------------------------------------------------------------------------------------------
+          $r8 = ["name"=> "Si", "y" => $this->p4->pluck('val1')[0]*100];
+          $r9 = ["name"=> "No", "y" => $this->p4->pluck('val2')[0]*100];
+          $r10 = ["name"=> "No", "y" => $this->p4->pluck('val3')[0]*100];
+          $pregunta4["chart"] = ["type" => "pie"];
+          $pregunta4["title"] = ["text" => "¿Qué tan beneficioso considera usted que puede llegar a tener  una persona con limitaciones ya sea parálisis u otros , si se llegaran a desarrollar esta tecnología?"];
+          $pregunta4["tooltip"] = ["pointFormat" =>  "{series.name}: <b>{point.percentage:.1f}%</b>"];
+          $pregunta4["plotOptions"]  = [
+            "pie" => [
+              "allowPointSelect" => true,
+              "cursor" => "pointer",
+              "dataLabels" => ["enabled" => false],
+              "showInLegend" => "true"]
+            ];
+            $pregunta4["series"] = [["name" => "Respuestas", "data" => [$r8, $r9, $r10]]];
 
-      return view('home', compact('yourFirstChart', 'yourSecondChart'));
-    }
-  }
+            return view('home', compact('pregunta1', 'pregunta2', 'pregunta3', 'pregunta4'));
+          }
+        }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,38 +4,59 @@
 <script src="/js/highcharts.js" charset="utf-8"></script>
 
 <div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">Pregunta 1</div>
-
-                <div class="panel-body">
-                  <div id="container"></div>
-                </div>
-            </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">Pregunta 1</div>
+        <div class="panel-body">
+          <div id="container"></div>
         </div>
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">Pregunta 2</div>
-
-                <div class="panel-body">
-                  <div id="container2"></div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">Pregunta 2</div>
+        <div class="panel-body">
+          <div id="container2"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">Pregunta 3</div>
+        <div class="panel-body">
+          <div id="container3"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">Pregunta 4</div>
+        <div class="panel-body">
+          <div id="container4"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
 <script type="text/javascript">
-    $(function () {
-      $(document).ready(function () {
-        $('#container').highcharts(
-            {!! json_encode($yourFirstChart) !!}
-        );
-        $('#container2').highcharts(
-            {!! json_encode($yourSecondChart) !!}
-        );
-      });
-    });
+$(function () {
+  $(document).ready(function () {
+    $('#container').highcharts(
+      {!! json_encode($pregunta1) !!}
+    );
+    $('#container2').highcharts(
+      {!! json_encode($pregunta2) !!}
+    );
+    $('#container3').highcharts(
+      {!! json_encode($pregunta3) !!}
+    );
+    $('#container4').highcharts(
+      {!! json_encode($pregunta4) !!}
+    );
+  });
+});
 </script>
 @endsection
